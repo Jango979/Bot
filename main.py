a38c53544d6b2395f278da2236ec06594fe43f6e
Jango979 <bravoac2016@licifug.ugto.mx>
2021-08-11 17:35:19 -0500

    Efectuado para 40

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import Passworder as ps
 from pandas import read_csv
-import parte1
+from parte1 import parte1
 
 
-def main(Numero_de_Bots=100):
+def main(Numero_de_Bots=40):
     ps.FakeUser(Numero_de_Bots)
     DF = read_csv('BotList.csv', header=None)
     parte1(DF)
 
 
 if __name__ == '__main__':
-    main(100)
+    main(40)
 
 
